4ea1602e6d74c6a5940a58ae5b7abd68063bd4d0
John Moore <jawnmoore@gmail.com>
2022-04-11 01:24:05 -0500

    Revamped website

--- js/script.js
+++ js/script.js
@@ -1,123 +1,24 @@
+var videos = [
+  '1.mp4',
+  '3.mp4',
+  '2.mp4'
+];
 
-// COMMENT OUT FOR TESTING DROP BOX
-// // //
-// Function to remove preload class
-//window.addEventListener('load', () => {
-//  const preload = document.querySelector('.preload');
-//  preload.classList.add('preload-finish');
-//});
+var index = Math.floor(Math.random() * videos.length);
+var html='<div><video width="100%" height="100%" src="media/video/' + videos[index] + '</video></div>';
+document.write(html);
 
+$(document).ready(function() {
+  $('#content').load('/home.html');
 
-$(document).ready(() => {
-  /* sticky navbar */
-  window.onscroll = function() {myFunction()};
-
-  var navbar = document.getElementById("navbar");
-  var sticky = navbar.offsetTop;
-
-  function myFunction() {
-    if (window.pageYOffset >= sticky) {
-      navbar.classList.add("sticky")
-    } else {
-      navbar.classList.remove("sticky");
-    }
-  }
-
-
-  /****************/
-  /* FOR ROTATION */
-  /****************/
-  var $ltr = $('.rotate');
-  var $win = $(window);
-
-  $win.on('scroll', function() {
-    var top = $win.scrollTop() * 1.8;
-    if (top > 360){
-      top = 360
-    }
-    $ltr.css('transform', 'rotate(' + top + 'deg)');
-  });
-
-  /***************/
-  /* FOR SCALING */
-  /***************/
-  var $ltr2 = $('.scale');
-
-  $win.on('scroll', function() {
-    var grow = (($win.scrollTop() / 64) / 2) + 1;
-    //$ltr2.css('transform', 'scale(' + grow + ')');
-    $ltr2.css('transform', 'scale(' + grow + ')');
-  });
-
-  /***************/
-  /* FOR SPACING */
-  /***************/
-  var $ltr3 = $('.push');
-
-  $win.on('scroll', function () {
-    var topo = $win.scrollTop() / 6;
-    $ltr3.css('letter-spacing', topo);
-  });
-
-  /*******************/
-  /* FOR 'M' SPACING */
-  /*******************/
-  var $m = $('.m');
-
-  $win.on('scroll', function () {
-    var topoo = $win.scrollTop() / 2;
-    $m.css('padding-top', topoo);
-  });
-
-
-  /**************************/
-  /*** TITLE TEXT SPACING ***/
-  /**************************/
-
-  $('.title').on('mouseenter', (event) => {
-      $(event.currentTarget).addClass('title-active')
-    }).on('mouseleave', event => {
-      $(event.currentTarget).removeClass('title-active')
-  });
-
-  /*************************/
-  /*** ROTATION ANIMATION **/
-  /*************************/
-
-  $('.icon').on('click', event => {
-    $(event.currentTarget).rotate({ count:4, duration:0.6, easing:'ease-out' });
+  $('#js-navigation a').click(function(e) {
+    e.preventDefault();
+    $("#content").load(e.target.href);
   })
+});
 
+const setTheme = theme => document.documentElement.className = theme;
 
-  /****************/
-  /*** ANIMATION **/
-  /****************/
-
-  var $desc = $('.desc');
-  $desc.waypoint(function (direction) {
-    if (direction == 'down') {
-      $desc.addClass('desc-animate');
-    } else {
-      $desc.removeClass('desc-animate');
-    }
-  }, {offset: '80%'});
-
-  var $ghost = $('.ghost');
-  $ghost.waypoint(function (direction) {
-    if (direction == 'down') {
-      $ghost.addClass('ghost-animate');
-    } else {
-      $ghost.removeClass('ghost-animate');
-    }
-  }, {offset: '80%'});
-
-  /******************************/
-  /* offset for navbar jumping */
-  /******************************/
-  var shiftWindow = function() { scrollBy(0, -47) };
-  window.addEventListener("hashchange", shiftWindow);
-  function load() {
-    if (window.location.hash) shiftWindow();
-  }
-
+document.getElementById('theme-select').addEventListener('change', function() {
+  setTheme(this.value);
 });
